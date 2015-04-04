9a1759e604847286a50adac3a20cfa093711a0cb
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-04-04 09:26:01 +0200

    no message

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-airports
+Airports
 ========
 
 A simple webapp to find the nearest airports to a location and their IATA codes.
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var restify = require("restify"); 
 var mongodb = require("mongodb");
 var fs 		= require("fs");
+var db 		= require("./db");
 
 var mongoClient = mongodb.MongoClient;
 var server = restify.createServer({
@@ -8,40 +9,41 @@ var server = restify.createServer({
 });
 server.use(restify.queryParser());
 
-// home
-server.get("/", function(req, res, next) {
-	fs.readFile("/var/www/airports/index.html", "utf8", function (err, body) {
-		if (err) throw err;
-		  
-		// set up header
-		res.writeHead(200, {
-			"Content-Length": Buffer.byteLength(body),
-			"Content-Type"	: "text/html"
+mongoClient.connect(db.config.host, function(err, db) {
+
+	if(err) throw err;
+	var airportsCol = db.collection("airports");
+
+	// home
+	server.get("/", function(req, res, next) {
+		fs.readFile("/var/www/airports/index.html", "utf8", function (err, body) {
+			if (err) throw err;
+			  
+			// set up header
+			res.writeHead(200, {
+				"Content-Length": Buffer.byteLength(body),
+				"Content-Type"	: "text/html"
+			});
+
+			// spit out html
+			res.write(body);
+			res.end();
+			return;
 		});
-
-		// spit out html
-		res.write(body);
-		res.end();
-		return;
 	});
-});
 
-// closest
-server.get("/closest", function (req, res, next) {
+	// closest
+	server.get("/closest", function (req, res, next) {
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	    res.header("Access-Control-Allow-Origin", "*");
+	    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+	    res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-	var latitude = parseFloat(req.query.lat);
-	var longitude = parseFloat(req.query.lon);
-	var location = [latitude, longitude];
+		var latitude = parseFloat(req.query.lat);
+		var longitude = parseFloat(req.query.lon);
+		var location = [latitude, longitude];
 
-	mongoClient.connect("mongodb://localhost:27017/airports/", function(err, db) {
-	    if(err) throw err;
-
-	    // insert item into collection
-	    var airportsCol = db.collection("airports");
+	    // find closest airport
 	    airportsCol.find({
 	    	"Coordinate": {"$near": location}
 	    }, {"limit": 30}).toArray(function(err, airports) {
@@ -50,8 +52,8 @@ server.get("/closest", function (req, res, next) {
 		    res.send(airports);
 	    });
 	});
-});
 
-server.listen(8041, function() {
-    console.log('%s listening at %s, love & peace', server.name, server.url);
+	server.listen(8041, function() {
+	    console.log('%s listening at %s, love & peace', server.name, server.url);
+	});
 });
